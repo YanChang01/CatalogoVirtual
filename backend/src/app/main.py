c2d1fae7354fc0bd4883cb3598b39fb4400dcd37
Yan <yanbackend@gmail.com>
2026-07-04 11:36:11 -0400

    feat: backend/src/app/main.py

---
+++ backend/src/app/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+
+from core.config import settings
+from routers import users, categorys, products
+
+# ================= APP FASTAPI =================
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    description=settings.PROJECT_DESCRIPTION,
+    version=settings.PROJECT_VERSION,
+)
+
+# Routers
+app.include_router(users.router)
+app.include_router(categorys.router)
+app.include_router(products.router)
+
+# Middlewares
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# EndPoints
+@app.get("/", status_code=status.HTTP_200_OK)
+async def root() -> dict:
+    return {
+        "título": app.title,
+        "descripción": app.description,
+        "versión": app.version,
+    }
+    
+
+
+
