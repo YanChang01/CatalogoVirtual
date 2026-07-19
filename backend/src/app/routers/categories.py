9a15a90b75bbee88731bf15df1cc8fa9b8995a0d
Yan <yanbackend@gmail.com>
2026-07-19 12:42:33 -0400

    feat: backend/src/app/routers/categories.py

--- backend/src/app/routers/categories.py
+++ backend/src/app/routers/categories.py
@@ -16,8 +16,25 @@ async def create(category: CategoryCreate, session: AsyncSession = Depends(get_a
     
     return await create_category(category=category, session=session)
 
+@router.get("/read/{name}", status_code=status.HTTP_200_OK, response_model=CategoryResponse)
+async def read(name: str, session: AsyncSession = Depends(get_async_session)) -> CategoryResponse:
+    
+    return await read_category(name=name, session=session)
 
+@router.get("/read", status_code=status.HTTP_200_OK, response_model=List[CategoryResponse])
+async def read2(session: AsyncSession = Depends(get_async_session)) -> CategoryResponse:
+    
+    return await read_categories(session=session)
 
+@router.put("/update/{name}", status_code=status.HTTP_200_OK, response_model=CategoryResponse)
+async def update(name: str, category: CategoryUpdate, session: AsyncSession = Depends(get_async_session)) -> CategoryResponse:
+    
+    return update_category(name=name, category=category, session=session)
+
+@router.delete("/delete/{name}", status_code=status.HTTP_200_OK, response_model=CategoryResponse)
+async def delete(name: str, session: AsyncSession = Depends(get_async_session)) -> CategoryResponse:
+    
+    return delete_category(name=name, session=session)
 
 
 
