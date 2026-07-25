af9e2b150d584880e15b43d4177d3b9c6af1003e
Yan <yanbackend@gmail.com>
2026-07-25 10:37:14 -0400

    feat: backend/src/app/routers/products.py

--- backend/src/app/routers/products.py
+++ backend/src/app/routers/products.py
@@ -4,18 +4,21 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 from models.models import Product
 from schemas.schemas import ProductCreate, ProductResponse, ProductUpdate, ProductWithCategory
-from crud.products import create_product, read_product, read_products, update_product, delete_product
+from crud.products import create_product, read_product, read_products, read_product_deleted, read_products_deleted, update_product, delete_product
 from core.client import get_async_session
 
 #Router
 router = APIRouter(prefix="/products")
 
 #EndPoints
+
+#POST
 @router.post("/create", status_code=status.HTTP_201_CREATED, response_model=ProductResponse)
 async def create(product: ProductCreate, session: AsyncSession = Depends(get_async_session)) -> ProductResponse:
     
     return await create_product(product=product, session=session)
 
+#GET
 @router.get("/read/{name}", status_code=status.HTTP_200_OK, response_model=ProductResponse)
 async def read(name: str, session: AsyncSession = Depends(get_async_session)) -> ProductResponse:
     
@@ -26,11 +29,23 @@ async def read2(session: AsyncSession = Depends(get_async_session)) -> List[Prod
     
     return await read_products(session=session)
 
+@router.get("/read/deleted/{name}", status_code=status.HTTP_200_OK, response_model=ProductResponse)
+async def read3(name: str, session: AsyncSession = Depends(get_async_session)) -> ProductResponse:
+    
+    return await read_product_deleted(name=name, session=session)
+
+@router.get("/read/deleted", status_code=status.HTTP_200_OK, response_model=List[ProductResponse])
+async def read4(session: AsyncSession = Depends(get_async_session)) -> List[ProductResponse]:
+    
+    return await read_products_deleted(session=session)
+
+#PUT
 @router.put("/update/{name}", status_code=status.HTTP_200_OK, response_model=ProductResponse)
 async def update(name: str, product: ProductUpdate, session: AsyncSession = Depends(get_async_session)) -> ProductResponse:
     
     return await update_product(name=name, product=product, session=session)
 
+#DELETE
 @router.delete("/delete/{name}", status_code=status.HTTP_200_OK, response_model=ProductResponse)
 async def delete(name: str, session: AsyncSession = Depends(get_async_session)) -> ProductResponse:
     
