2ab6c67d941134a4466b42b577d1373e40234044
Yan <yanbackend@gmail.com>
2026-07-25 10:34:27 -0400

    feat: backend/src/app/routers/users.py

--- backend/src/app/routers/users.py
+++ backend/src/app/routers/users.py
@@ -5,18 +5,21 @@ from pydantic import EmailStr
 
 from models.models import User
 from schemas.schemas import UserCreate, UserResponse, UserUpdate
-from crud.users import create_user, read_user, read_users, update_user, delete_user
+from crud.users import create_user, read_user, read_users, read_user_deleted, read_users_deleted, update_user, delete_user
 from core.client import get_async_session
 
 #Router
 router = APIRouter(prefix="/users")
 
 #EndPoints
+
+#POST
 @router.post("/create", status_code=status.HTTP_201_CREATED, response_model=UserResponse)
 async def create(user: UserCreate, session: AsyncSession = Depends(get_async_session)) -> UserResponse:
     
     return await create_user(user=user, session=session)
 
+#GET
 @router.get("/read/{email}", status_code=status.HTTP_200_OK, response_model=UserResponse)
 async def read(email: EmailStr, session: AsyncSession = Depends(get_async_session)) -> UserResponse:
     
@@ -27,11 +30,23 @@ async def read2(session: AsyncSession = Depends(get_async_session)) -> List[User
     
     return await read_users(session=session)
 
+@router.get("/read/deleted/{email}", status_code=status.HTTP_200_OK, response_model=UserResponse)
+async def read3(email: EmailStr, session: AsyncSession = Depends(get_async_session)) -> UserResponse:
+    
+    return await read_user_deleted(email=email, session=session)
+
+@router.get("/read/deleted", status_code=status.HTTP_200_OK, response_model=List[UserResponse])
+async def read4(session: AsyncSession = Depends(get_async_session)) -> List[UserResponse]:
+    
+    return await read_users_deleted(session=session)
+
+#PUT
 @router.put("/update/{email}", status_code=status.HTTP_200_OK, response_model=UserResponse)
 async def update(email: EmailStr, user: UserUpdate, session: AsyncSession = Depends(get_async_session)) -> UserResponse:
     
     return await update_user(email=email, user=user, session=session)
 
+#DELETE
 @router.delete("/delete/{email}", status_code=status.HTTP_200_OK, response_model=UserResponse)
 async def delete(email: EmailStr, session: AsyncSession = Depends(get_async_session)) -> UserResponse:
     
