f487fb1b6ad77ae37804ff32764e45fe35f31ef9
Yan <yanbackend@gmail.com>
2026-09-10 15:29:02 -0400

    Añado localhost a CORS para pruebas locales

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -22,7 +22,11 @@ app.include_router(my_cloudinary_module.router)
 # Middlewares
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL],
+    allow_origins=[
+        settings.FRONTEND_URL,
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
