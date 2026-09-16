cc20db3cce7df2454dee970fa5441ca2700fb934
Yan <yanbackend@gmail.com>
2026-09-16 13:38:42 -0400

    Actualizando el CORS para deploy

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -22,11 +22,7 @@ app.include_router(my_cloudinary_module.router)
 # Middlewares
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        settings.FRONTEND_URL,
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=[settings.FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
